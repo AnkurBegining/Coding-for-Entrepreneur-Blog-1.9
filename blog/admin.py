63fc90c7810958f420f1dd5f4c4e88c0adad0310
AnkurBegining <201551016.iiitvadodara.ac.in@gmail.com>
2017-12-18 15:39:49 +0530

    customized code for admin added

--- blog/admin.py
+++ blog/admin.py
@@ -3,5 +3,25 @@ from blog.models import post
 
 # Register your models here.
 
-admin.site.register(post)
+class postModelAdmin(admin.ModelAdmin):
+    # for displaying more thing in admin page
+    list_display = ['title','timestamp','updated']
+
+    # for displaying link with another attribute
+    list_display_links = ['timestamp']
+
+    # for displaying attribute with editable features
+    list_editable = ['title']
+
+    # for list filter
+    list_filter = ['updated', 'timestamp']
+
+    # setting search field
+    search_fields = ['title','post_content']
+
+    class Meta:
+        model = post
+
+
+admin.site.register(post,postModelAdmin)
 
