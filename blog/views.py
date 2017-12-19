6b6171c507a0895ce0bc91bbb09ead99736562b3
AnkurBegining <201551016.iiitvadodara.ac.in@gmail.com>
2017-12-19 11:56:44 +0530

    Dynamic url router for post_detail  added

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,33 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from blog.models import post
 
 # Create your views here.
+
+def post_list(request):
+    posts = post.objects.all()
+    context_data={
+        'posts_list': posts
+    }
+    return render(request, "blog/post_list.html", context_data)
+
+
+def post_create(request):
+    return render(request, "blog/post_create.html", {})
+
+
+def post_detail(request, id):
+
+    instance = get_object_or_404(post, id =id)
+    context_data = {
+        "title" : instance.title,
+        "post" : instance
+    }
+    return render(request, "blog/post_detail.html", context_data)
+
+
+def post_update(request):
+    return render(request, "blog/post_update.html", {})
+
+
+def post_delete(request):
+    return render(request, "blog/post_delete.html", {})
